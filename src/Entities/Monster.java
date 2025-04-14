f7a64b4f9a0db9f79a9d120f7d88d31920b83ac5
Liborio <felipesouzaliborio@gmail.com>
2025-04-14 14:53:41 -0300

    feat: monster class

---
+++ src/Entities/Monster.java
@@ -0,0 +1,24 @@
+package Entities;
+
+import Abilities.Ability;
+import DataStructure.Lista;
+
+public class Monster extends Entidade{
+
+    private int xpDrop;
+
+    public Monster(Lista<Ability> habilidades, int manaAtual, int manaMaxima, int vidaAtual, int vidaMaxima, int nivel, String nome, int id, int xpDrop) {
+        super(habilidades, manaAtual, manaMaxima, vidaAtual, vidaMaxima, nivel, nome, id);
+        this.xpDrop = xpDrop;
+    }
+
+    public int getXpDrop() {
+        return xpDrop;
+    }
+
+    public void setXpDrop(int xpDrop) {
+        this.xpDrop = xpDrop;
+    }
+
+
+}
